6abf6d79491abf33e26d481ab269129fc61db321
Yogesh <yogeshborra058@gmail.com>
2022-08-05 16:46:17 +0530

    need to push route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,9 +3,11 @@ const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+
 });
 
 
