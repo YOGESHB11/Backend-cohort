41e9f481f46969873e5372cdd22794b1ab858a07
Yogesh <yogeshborra058@gmail.com>
2022-09-02 01:31:42 +0530

    axios assignment - 01.09.2022

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
+const WeatherController = require("../controllers/weatherController")
+const MemeController = require("../controllers/memeController")
 
-
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
+//-------------------------------(vaccine)-----------------------------------------------
 
 router.get("/cowin/states", CowinController.getStates)
+
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+
 router.get("/cowin/getByPin", CowinController.getByPin)
+
+router.get("/cowin/session",CowinController.getVaccineSession)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+//---------------------------------(weather)---------------------------------------------
 
+router.get("/weather/london", WeatherController.londonWeather)
 
+router.get("/weather/london/temp",WeatherController.londonTemp)
+
+router.get("/weather/city" , WeatherController.listofCities)
+
+//-----------------------------------{memes}--------------------------------------------------
+
+router.post("/memes" , MemeController.memeMaker)
 
 module.exports = router;
